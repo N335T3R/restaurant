7ab317aeb48aecf713de525bedddf57f84a42980
N335T3R <n335t3r@gmail.com>
2025-05-10 14:32:03 -0400

    About page assembled

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import { serveMenu } from "./modules/menu";
 import { serveCredits } from "./modules/credits";
 import { serveHome } from "./modules/home";
 
-serveHome();
-serveMenu();
-serveAbout();
-serveCredits();
+function serveSite() {
+    const homeBtn = document.getElementById('home-btn');
+    const menuBtn = document.getElementById('menu-btn');
+    const aboutBtn = document.getElementById('about-btn');
+    const creditsBtn = document.getElementById('credits-btn');
+
+    homeBtn.addEventListener('click', serveHome);
+    menuBtn.addEventListener('click', serveMenu);
+    aboutBtn.addEventListener('click', serveAbout);
+    creditsBtn.addEventListener('click', serveCredits);
+
+    serveHome();
+}
+serveSite();
--- src/modules/about.js
+++ src/modules/about.js
@@ -2,11 +2,30 @@ import cauldronPng from "../assets/images/cauldronBlue.png";
 import { changeCauldron } from "../utils/changeCauldron";
 
 export function serveAbout(div) {
-    document.querySelector('main').innerHTML = "";
+    const main = document.querySelector('main');
+    main.innerHTML = "";
     changeCauldron(document.getElementById('about-icon'), cauldronPng);
 
-    const bio = "The Leaky Cauldron was built in 1550 by Daisy Dodderidge. Prior to the institution of the International Statute of Secrecy, The Leaky Cauldron hosted both magical and non-magical guests. The Leaky Cauldron now serves as a tavern and pub to magical beings from all over the world.";
-    const location = "The Leaky Cauldron is located in London at #1 Diagon Alley. The Leaky Cauldron is connected to the Floo Network.";
-    const hours = "The Leaky Cauldron does not close";
+    const header = document.createElement('h2')
+    header.textContent = "About";
+    main.appendChild(header);
+
+    const info = {
+        bio: "The Leaky Cauldron was built in 1550 by Daisy Dodderidge. Prior to the institution of the International Statute of Secrecy, The Leaky Cauldron hosted both magical and non-magical guests. The Leaky Cauldron now serves as a tavern and pub to magical beings from all over the world.",
+        location: "The Leaky Cauldron is located in London at #1 Diagon Alley. The Leaky Cauldron is connected to the Floo Network.",
+        hours: "The Leaky Cauldron does not close"
+    }
+
+    const bio = document.createElement('p');
+    const location = document.createElement('p');
+    const hours = document.createElement('p');
+    bio.textContent = info.bio;
+    location.textContent = info.location;
+    hours.textContent = info.hours;
+    main.appendChild(bio);
+    main.appendChild(location);
+    main.appendChild(hours);
+
 }
+
 
